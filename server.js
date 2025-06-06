96beca11f8ced26ea47b809f2f6533938d9a2bfd
horus911 <34630037+horus911@users.noreply.github.com>
2025-06-06 13:24:46 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (ws) => {
+  console.log("Utilisateur connecté");
+  ws.on("message", msg => {
+    wss.clients.forEach(client => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(msg);
+      }
+    });
+  });
+});
+
+app.get("/", (req, res) => res.send("Serveur WS connecté"));
+server.listen(process.env.PORT || 3000);
